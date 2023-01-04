190a5376c7c54726e01bc1da926bcdce4963adb1
Morgan <morganjbare@gmail.com>
2023-01-04 18:20:16 +0100

    Added Triangle SVG and basic structure of hero section

---
+++ my-app/src/components/Hero/Hero.styled.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+import { ReactComponent as TriangleSVG } from '../../assets/SVG/Triangle-2.0.svg';
+
+
+export const HeroContainer = styled.div`
+    width: 500px;
+    height: 500px;
+    margin: 0 auto;
+`;
+
+export const StyledTriangle = styled(TriangleSVG)`
+    width: 320px;
+    height: 320px;
+    margin: 0 auto;
+`;
--- my-app/src/components/Hero/Hero.tsx
+++ my-app/src/components/Hero/Hero.tsx
@@ -1,8 +1,18 @@
 import React from 'react'
 
+import { 
+  HeroContainer,
+  StyledTriangle,
+ } from './Hero.styled';
+
 const Hero: React.FC = () => {
   return (
-    <div>Hello</div>
+    <HeroContainer>
+      <h2>Hello.</h2>
+      <h1>I'm Morgan</h1>
+      <p>Front-end developer based in Wiesbaden, Germany.</p>
+      <StyledTriangle/>
+    </HeroContainer>
   )
 }
 
