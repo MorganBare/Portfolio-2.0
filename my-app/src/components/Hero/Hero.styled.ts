9b5ad460261d523dccbc310e6483820d97ccf2b5
Morgan <morganjbare@gmail.com>
2023-01-08 15:37:16 +0100

    Reduced height of hero section

--- my-app/src/components/Hero/Hero.styled.ts
+++ my-app/src/components/Hero/Hero.styled.ts
@@ -5,7 +5,7 @@ import { ReactComponent as TriangleSVG } from '../../assets/SVG/Triangle-2.0.svg
 
 export const HeroContainer = styled.div`
     width: clamp(320px, 75%, 1200px);
-    height: 80vh;
+    height: 75vh;
     margin: 2rem auto 0 auto;
     padding: 1rem 1rem;
     position: relative;
